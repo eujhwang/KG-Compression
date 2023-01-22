45f79dd50be599caf53c35f96b99b585b53b8e03
Eunjeong Hwang <hej78520@gmail.com>
2023-01-22 02:59:09 -0800

    Update kgtrainer_utils.py

--- trainers/kgtrainer_utils.py
+++ trainers/kgtrainer_utils.py
@@ -752,7 +752,7 @@ def sinkhorn_loss(x, y, epsilon, mu, nu, n, m, p=2, niter=100, acc=1e-3, unbalan
         return torch.log(torch.exp(A).sum(1, keepdim=True) + 1e-6)  # add 10^-6 to prevent NaN
 
     # Actual Sinkhorn loop ......................................................................
-    u, v, err = torch.zeros_like(mu).to(device), torch.zeros_like(nu).to(device), 0.
+    u, v, err = torch.zeros_like(mu), torch.zeros_like(nu), 0.
     u.requires_grad = True
     v.requires_grad = True
     actual_nits = 0  # to check if algorithm terminates because of threshold or max iterations reached
@@ -780,6 +780,6 @@ def sinkhorn_loss(x, y, epsilon, mu, nu, n, m, p=2, niter=100, acc=1e-3, unbalan
 def sinkhorn_loss_default(x, y, epsilon=0.01, p=2, niter=100, device='cuda'):
     n = x.shape[0]
     m = y.shape[0]
-    mu = torch.ones(n).to(device)/n
-    nu = torch.ones(m).to(device)/m
+    mu = torch.ones(n)/n
+    nu = torch.ones(m)/m
     return sinkhorn_loss(x, y, epsilon, mu, nu, n, m, p, niter=niter, device=device)
