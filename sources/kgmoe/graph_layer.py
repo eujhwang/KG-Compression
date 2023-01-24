41c9faccedf2790c73671c4baf1e72b75646d7f0
Eunjeong Hwang <hej78520@gmail.com>
2023-01-24 04:21:05 -0800

    Update graph_layer.py

--- sources/kgmoe/graph_layer.py
+++ sources/kgmoe/graph_layer.py
@@ -131,6 +131,7 @@ class GraphEncoder(nn.Module):
         for i in range(layer_number):
             concept_hidden_list.append(concept_hidden)
             concept_hidden, relation_hidden = self.comp_gcn(concept_hidden, relation_hidden, head, tail, triple_label, i)
+        concept_hidden_list.append(concept_hidden)
         concept_hidden = torch.cat(concept_hidden_list, dim=-1)
         return concept_hidden, relation_hidden
 
@@ -331,6 +332,7 @@ class GraphEncoder(nn.Module):
         #     concept_hidden_list.append(concept_hidden)
         #     # relation_hidden_list.append(relation_hidden)
         node_repr, rel_repr = self.multi_layer_comp_gcn(memory, rel_repr, head, tail, triple_label, layer_number=self.hop_number)
+        print("node_repr:", node_repr.shape)
         # node_repr = torch.cat(concept_hidden_list, dim=-1).to(memory.device) # [bsz, #concepts, 768 * num_hop]
         node_repr = self.sag_pooling(node_repr, rel_repr, head, tail, relation, triple_label, adj)
         ###################################### end of sag_pooling ######################################
