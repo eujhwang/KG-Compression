40b54e0349d89bfd815fc43415044a1a29a9d3be
Eunjeong Hwang <hej78520@gmail.com>
2023-01-24 23:21:48 -0800

    Update kgmoe_trainer.py

--- trainers/kgmoe_trainer.py
+++ trainers/kgmoe_trainer.py
@@ -189,19 +189,21 @@ class KGMoESeq2SeqTrainer(Seq2SeqTrainer):
         opt_loss = 0.0
         epsilon = 1.0
         opt_epochs = 10
-        skipped = 0
+        skipped_mem = 0
+        skipped_new_mem = 0
         for i in range(len(node_output)):
             mem = self.get_nonzero_rows(node_output[i])
             new_mem = self.get_nonzero_rows(node_hidden[i])
             if mem.shape[0] == 0:
-                skipped += 1
+                skipped_mem += 1
                 continue
             if new_mem.shape[0] == 0:
-                skipped += 1
+                skipped_new_mem += 1
                 continue
             loss = sinkhorn_loss_default(mem, new_mem, epsilon, niter=opt_epochs, device=device).float()
             opt_loss += loss
-        print("skipped:", skipped)
+        print("skipped_mem:", skipped_mem)
+        print("skipped_new_mem:", skipped_new_mem)
         return opt_loss
 
     def get_nonzero_rows(self, M):  # M is a matrix
