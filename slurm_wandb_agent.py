1685555f791ee1e5dc11e0e4a0dffa9c49d31a1f
Eunjeong Hwang <hej78520@gmail.com>
2023-01-25 02:23:55 -0800

    Update slurm_wandb_agent.py

--- slurm_wandb_agent.py
+++ slurm_wandb_agent.py
@@ -13,26 +13,26 @@ def main(args):
             f.write('*')
 
     template_path = dict()
-    template_path['srun.sh'] = Path(slurm_dir / args.srun_template)
+    # template_path['srun.sh'] = Path(slurm_dir / args.srun_template)
     template_path['sbatch.sh'] = Path(slurm_dir / args.sbatch_template)
 
     template_str = dict()
-    template_str['srun.sh'] = '\n'.join((
-        "#!/bin/sh",
-        "export NO_PROGRESS_BAR=true",
-    ))
+    # template_str['srun.sh'] = '\n'.join((
+    #     "#!/bin/sh",
+    #     "export NO_PROGRESS_BAR=true",
+    # ))
     template_str['sbatch.sh'] = '\n'.join((
         "#!/bin/bash",
-        "#SBATCH --job-name=mokge-new",
         "#SBATCH --gres=gpu:1",
         "#SBATCH --qos=normal",
         "#SBATCH --partition=a40",
         "#SBATCH --cpus-per-task=8",
         "#SBATCH --mem-per-cpu=16GB",
-        "#SBATCH --time=7-00:00:00",
+        # "#SBATCH --time=7-00:00:00",
     ))
 
-    for fname in ['srun.sh', 'sbatch.sh']:
+    for fname in ['sbatch.sh']:
+    # for fname in ['srun.sh', 'sbatch.sh']:
         if not template_path[fname].exists():
             with open(template_path[fname], 'w') as f:
                 f.write(template_str[fname])
@@ -46,19 +46,19 @@ def main(args):
             raise ValueError(f'Got sweep_id = {args.sweep_id}, but directory {job_dir} already exists!')
 
     file_path = dict()
-    file_path['srun.sh'] = Path(job_dir / args.srun_filename)
+    # file_path['srun.sh'] = Path(job_dir / args.srun_filename)
     file_path['sbatch.sh'] = Path(job_dir / args.sbatch_filename)
 
     #The following might be nice but are definitely not necessary:
     #TODO: Allow SBATCH defaults to be overridden by pass-through arguments to this script
 
     file_str = dict()
-    file_str['srun.sh'] = "\n".join((
-        "",
-        f"wandb agent --count 10 {args.sweep_id}",
-        # f"pwd", # for testing
-        # f"./test.sh", # for testing
-    ))
+    # file_str['srun.sh'] = "\n".join((
+    #     "",
+    #     f"wandb agent --count 10 {args.sweep_id}",
+    #     # f"pwd", # for testing
+    #     # f"./test.sh", # for testing
+    # ))
 
     file_str['sbatch.sh'] = "\n".join((
         "",
@@ -69,16 +69,18 @@ def main(args):
         f'#SBATCH --array=1-{args.num_jobs}\n' if args.num_jobs > 1 else '',
         f"source ~/.bashrc",
         f"conda activate {args.conda_env}",
-        f"srun {file_path['srun.sh']}"
+        # f"srun {file_path['srun.sh']}",
+        f"wandb agent --count 10 {args.sweep_id}",
     ))
 
-    for fname in ['srun.sh', 'sbatch.sh']:
+    # for fname in ['srun.sh', 'sbatch.sh']:
+    for fname in ['sbatch.sh']:
         os.system(f"cp {template_path[fname]} {file_path[fname]}")
         with open(file_path[fname], 'a') as f:
             f.write(file_str[fname])
 
-    if args.edit_srun:
-        os.system(f"{args.editor} {file_path['srun.sh']}")
+    # if args.edit_srun:
+    #     os.system(f"{args.editor} {file_path['srun.sh']}")
     if args.edit_sbatch:
         os.system(f"{args.editor} {file_path['sbatch.sh']}")
     if not args.no_run:
@@ -91,13 +93,13 @@ if __name__ == '__main__':
     parser.add_argument('--num-jobs', type=int, default=1)
     parser.add_argument('--num-agents-per-job', type=int, default=1)
     parser.add_argument('--edit-sbatch', action='store_true', help="open the sbatch.sh file in a text editor before running")
-    parser.add_argument('--edit-srun', action='store_true', help="open the srun.sh file in a text editor before running")
+    # parser.add_argument('--edit-srun', action='store_true', help="open the srun.sh file in a text editor before running")
     parser.add_argument('--no-run', action='store_true', help="don't run the job(s), just create the sh files")
     parser.add_argument('-f', '--force', action='store_true', help="force creation of job folder if it already exists, potentially overwriting data")
     parser.add_argument('--slurm_dir', type=str, default="slurm", help="directory to store slurm files in")
-    parser.add_argument('--srun-filename', type=str, default="srun.sh", help="filename of srun script (to be created)")
+    # parser.add_argument('--srun-filename', type=str, default="srun.sh", help="filename of srun script (to be created)")
     parser.add_argument('--sbatch-filename', type=str, default="sbatch.sh", help="filename of sbatch script (to be created)")
-    parser.add_argument('--srun-template', type=str, default="srun.sh", help="filename of srun template (which will be copied and appended to) relative to SLURM_DIR")
+    # parser.add_argument('--srun-template', type=str, default="srun.sh", help="filename of srun template (which will be copied and appended to) relative to SLURM_DIR")
     parser.add_argument('--sbatch-template', type=str, default="sbatch.sh", help="filename of sbatch template (which will be copied and appended to) relative to SLURM_DIR")
     parser.add_argument('--editor', type=str, default="vim")
     # More generally, should be able to use EDITOR = "${EDITOR:-vim}" but I don't always have my editor set.
