931ec0d2efc07c665f069525a8384e6170ed69b7
Eunjeong Hwang <hej78520@gmail.com>
2023-02-26 19:53:27 -0800

    Update comet_inference.py

--- comet/comet_inference.py
+++ comet/comet_inference.py
@@ -200,17 +200,16 @@ def _augment_kg(kg, hop_concepts, hop_index, hop_distances, _relation2id, _id2re
 def augment_kg_triples(args, kgs):
     print("start augmenting kg triples...")
 
+    model, sampler, data_loader, text_encoder = load_comet(args)
+
     if torch.cuda.is_available():
         multiprocessing.set_start_method('spawn', force=True)
         pool = multiprocessing.get_context('spawn').Pool(args.num_proc)
+        model.share_memory()
     else:
         pool = multiprocessing.Pool(args.num_proc)
     procs = []
     start_time = time.perf_counter()
-    
-    model, sampler, data_loader, text_encoder = load_comet(args)
-    # print("number of cpu:", multiprocessing.cpu_count())
-    # assert False
 
     conceptnet_relations = data.conceptnet_data.conceptnet_relations
     _relation2id = dict()
